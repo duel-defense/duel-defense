a5cbc1737f56154f0b65e224879b18b027e674af
d10sfan <d10sfan@gmail.com>
2023-09-27 20:48:18 -0500

    Add touch support for build mode

--- Scenes/MainScenes/GameScene.gd
+++ Scenes/MainScenes/GameScene.gd
@@ -134,6 +134,11 @@ func _unhandled_input(event):
 		if (movement):
 			controller_mouse_movement += movement
 			update_tower_preview(controller_mouse_movement)
+	elif build_mode and event is InputEventScreenTouch:
+		update_tower_preview(event.global_position)
+		
+		if event.double_tap:
+			verify_and_build()
 		
 ### build functions
 
