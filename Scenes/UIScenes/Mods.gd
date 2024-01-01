0b2512b29f980dd94af67768b7a6609e6e57b4ec
d10sfan <d10sfan@gmail.com>
2024-01-01 09:01:30 -0600

    Hide available mods label if no mods.io mods found

--- Scenes/UIScenes/Mods.gd
+++ Scenes/UIScenes/Mods.gd
@@ -10,7 +10,7 @@ func open():
 	
 func populate_mods():
 	for item in mods_container.get_children():
-		item.queue_free()
+		item.free()
 		
 	var mods_loaded = configuration_manager.mod_manager.mods_loaded
 	for mod in mods_loaded:
@@ -22,17 +22,16 @@ func populate_mods():
 		new_mod_scene.mod_data = mod
 		new_mod_scene.mod_loaded = true
 		mods_container.add_child(new_mod_scene)
-		
-	var mod_scene_label = mod_scene.instantiate()
-	mod_scene_label.mod_label = "Available mod.io Mods"
-	mod_scene_label.mod_divider_label = true
-	mods_container.add_child(mod_scene_label)
 	
-	var mods_available = configuration_manager.mod_manager.available_mods
+	var mods_available = configuration_manager.mod_manager.available_mods_for_ui()
+	
+	if len(mods_available) > 0:
+		var mod_scene_label = mod_scene.instantiate()
+		mod_scene_label.mod_label = "Available mod.io Mods"
+		mod_scene_label.mod_divider_label = true
+		mods_container.add_child(mod_scene_label)
+	
 	for mod in mods_available:
-		var mod_file_loaded = configuration_manager.fs.file_exists(configuration_manager.mod_manager.user_mods_path + mod.modfile_name)
-		if mod_file_loaded:
-			continue
 		var new_mod_scene = mod_scene.instantiate()
 		new_mod_scene.mod_label = mod.name
 		new_mod_scene.mod_author = "by %s" % mod.submitted_by_username
@@ -40,6 +39,14 @@ func populate_mods():
 		new_mod_scene.fs = configuration_manager.fs
 		new_mod_scene.mod_data = mod
 		mods_container.add_child(new_mod_scene)
+		
+	print(OS.has_feature("editor"))
+	print(len(mods_container.get_children()))
+	if len(mods_container.get_children()) == 0 and OS.has_feature("editor"):
+		var mod_scene_label = mod_scene.instantiate()
+		mod_scene_label.mod_label = "Mods do not load in editor"
+		mod_scene_label.mod_divider_label = true
+		mods_container.add_child(mod_scene_label)
 
 func _on_close_requested():
 	visible = false
